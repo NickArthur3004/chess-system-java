c8057a9d96259dffa37d93f84ad12f4c238c6bbd
NickArthur3004 <nicolas.arthur2786@gmail.com>
2020-12-16 12:59:47 -0300

    Implementing possible moves of King

--- src/chess/Peça/Rei.java
+++ src/chess/Peça/Rei.java
@@ -1,5 +1,6 @@
 package chess.Peça;
 
+import TabuleiroGame.Posicao;
 import TabuleiroGame.Tabuleiro;
 import chess.ChessPeça;
 import chess.Color;
@@ -15,9 +16,71 @@ public class Rei extends ChessPeça {
 	public String toString() {
 		return "K";
 	}
+	
+	private boolean canMove(Posicao posicao) {
+		ChessPeça p = (ChessPeça)getTabuleiro().peça(posicao);
+		return p == null || p.getColor() != getColor();
+	}
+	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+		
+		Posicao p = new Posicao(0, 0);
+		
+		//cima
+		p.setValues(posicao.getLinha() - 1, posicao.getColuna());
+		if(getTabuleiro().PosicaoExistente(p) && canMove(p) ) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			
+		}
+		
+		//baixo
+		p.setValues(posicao.getLinha() + 1, posicao.getColuna());
+		if(getTabuleiro().PosicaoExistente(p) && canMove(p) ) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			
+		}
+		
+		//esquerda
+		p.setValues(posicao.getLinha(), posicao.getColuna()- 1);
+		if(getTabuleiro().PosicaoExistente(p) && canMove(p) ) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			
+		}
+		
+		//direita
+		p.setValues(posicao.getLinha(), posicao.getColuna()+ 1);
+		if(getTabuleiro().PosicaoExistente(p) && canMove(p) ) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			
+		}
+		//diagonal cima esquerda
+		p.setValues(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		if(getTabuleiro().PosicaoExistente(p) && canMove(p) ) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			
+		}
+		
+		//diagonal cima direita
+		p.setValues(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		if(getTabuleiro().PosicaoExistente(p) && canMove(p) ) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			
+		}
+		//diagonal baixo esquerda
+		p.setValues(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		if(getTabuleiro().PosicaoExistente(p) && canMove(p) ) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			
+		}
+		
+		//diagonal baixo direita
+		p.setValues(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		if(getTabuleiro().PosicaoExistente(p) && canMove(p) ) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			
+		}
 		return mat;
 	}
 
